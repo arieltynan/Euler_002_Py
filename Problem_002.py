e0f678dc5214aa73ff963802bcafe6c66b253601
Ariel Tynan <90798242+arieltynan@users.noreply.github.com>
2022-01-23 14:39:44 -0700

    Add files via upload

---
+++ Problem_002.py
@@ -0,0 +1,18 @@
+#Even Fibonacci numbers
+
+
+import math
+
+total = 0
+numb1 = 1 #first val added
+numb2 = 1 #second val added
+numb3 = 0 #sum
+while numb3 < 4000000: #fibonacci loop
+    numb3 = numb1 + numb2
+    if numb3 < 4000000:
+        numb2 = numb1 #swap
+        numb1 = numb3 #swap
+
+        if numb3 % 2 == 0:
+            total = total + numb3
+print(total)
